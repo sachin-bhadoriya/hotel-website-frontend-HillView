24cec9122ea4944d92a41128fc211f73288b2074
sachin <sachin@singh.co.in>
2025-05-15 02:24:43 +0530

    work

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import './App.css'
-import ScrollToTop from "./components/JSXFiles/OtherElements/ScrollToTop";
+import ScrollToTop from "./components/JSXFiles/OtherElements/ScrollToTop.jsx";
 import NewNavbar from './Components/JSXFiles/OtherElements/NewNavbar';
 import CookieConsent from './Components/JSXFiles/OtherElements/CookieConsent';
 
